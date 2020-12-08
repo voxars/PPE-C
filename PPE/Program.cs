d08dfde2d2397dcf01445540aa30535c3992f9d5
Amaleo1630 <60462731+Amaleo1630@users.noreply.github.com>
2020-12-08 16:48:57 +0100

    .

--- PPE/Program.cs
+++ PPE/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,14 +9,15 @@ namespace PPE
     static class Program
     {
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PPE());
+            Application.Run(new Form1());
         }
     }
 }
