410273d999940e06c18b0414a24ca2f5ef1adc57
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-12-08 15:20:57 +0100

    -

---
+++ PPE-C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PPE_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
