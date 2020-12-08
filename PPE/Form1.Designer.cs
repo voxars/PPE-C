79d62b5df0288c5eae03c51db34520c1054ef82d
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-12-08 17:37:11 +0100

    -

--- PPE/Form1.Designer.cs
+++ PPE/Form1.Designer.cs
@@ -1,16 +1,17 @@
-﻿namespace PPE
+﻿
+namespace PPE
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Variable nécessaire au concepteur.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Nettoyage des ressources utilisées.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,11 +21,11 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Code généré par le Concepteur Windows Form
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
         /// </summary>
         private void InitializeComponent()
         {
