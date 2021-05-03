c0b8500861d34127d1324850184c5b39eabaa270
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2021-05-03 17:30:02 +0200

    -

---
+++ PPE/Login.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+
+namespace PPE
+{
+    public class Login
+    {
+        public static bool testLogin(string log, string pass)
+        {
+            DAOFactory dao = new DAOFactory();
+            dao.connecter();
+            string requette = "SELECT * FROM participant WHERE login = '" + log + "' AND password = '" + pass + "'";
+
+            SqlDataReader lecteur = dao.excecSQLRead(requette);
+
+            int count = 0;
+            while (lecteur.Read())
+            {
+                count++;
+            }
+
+            if (count != 0)
+            {
+                return true;
+            }
+
+            return false;
+
+
+        }
+    }
+}
