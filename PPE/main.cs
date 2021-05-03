b2758ebb44d0a65d3830b7335c5dd670579d151c
Amaleo1630 <60462731+Amaleo1630@users.noreply.github.com>
2021-05-03 17:45:15 +0200

    o

--- PPE/main.cs
+++ PPE/main.cs
@@ -13,77 +13,47 @@ namespace PPE
     public partial class main : Form
     {
         private Atelier monAtelier;
-        private Animateur monAnimateur;
-
         public main()
         {
             InitializeComponent();
         }
 
-        #region initialisation des données
         private void initialiserAtelier()
         {
             monAtelier = new Atelier(10, "test", 50, "21:20", "21:30");
 
             monAtelier.LesAteliers = Atelier.listeAtelier();
         }
-        private void initialiserAnimateur()
-        {
-            monAnimateur = new Animateur(1, "testAnimateur", "", "", "","",1);
 
-            monAnimateur.LesAnimateurs = Animateur.listeAnimateur();
-        }
-        #endregion
-
-
-        private void main_Load(object sender, EventArgs e)
-        {
-            initialiserAtelier();
-            initialiserAnimateur();
-
-            foreach (Atelier unAtelier in monAtelier.LesAteliers)
-            {
-                cbxAtelier.Items.Add(unAtelier.Libelle);
-                cbxAtelierAnimateur.Items.Add(unAtelier.Libelle);
-            }
-
-            foreach (Animateur unAnimateur in monAnimateur.LesAnimateurs)
-            {
-                cbxAnimateur.Items.Add(unAnimateur.Prenom);
-            }
-
-        }
-
-        private void cbxAtelier_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int i = cbxAtelier.SelectedIndex;
-            Atelier unAtelier;
-            unAtelier = monAtelier.LesAteliers.ElementAt(i);
-
-            lblNomAtelier.Text = unAtelier.Libelle;
-            lblHoraireDebut.Text = unAtelier.Debut.ToString();
-            lblHoraireFin.Text = unAtelier.Fin.ToString();
-        }
-
-        #region not use
-        private void cbxAtelierAnimateur_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void cbxAnimateur_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void horaireAtelierToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            initialiserAtelier();
+        }
         private void tabPageHoraire_Enter(object sender, EventArgs e)
         {
-
+            foreach (Atelier unCompte in monAtelier.LesAteliers)
+            {
+                cbxAtelier.Items.Add(unCompte.Libelle);
+            }
         }
-        #endregion
+
+
+        private void cbxAtelier_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = cbxAtelier.SelectedIndex;
+            Atelier unAtelier;
+
+            unAtelier = monAtelier.LesAteliers.ElementAt(i);
+            lblNomAtelier.Text = unAtelier.Libelle;
+            lblHoraireDebut.Text = unAtelier.Debut.ToString();
+            lblHoraireFin.Text = unAtelier.Fin.ToString();
+        }
+
     }
 }
