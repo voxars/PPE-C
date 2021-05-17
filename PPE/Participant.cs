1f96349e9a8b0a3ce4f3d4552fb1b41906341e95
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2021-05-17 19:35:22 +0200

    -

--- PPE/Participant.cs
+++ PPE/Participant.cs
@@ -1,47 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PPE
+﻿namespace PPE
 {
-    class Participant
+    public class participant
     {
-        private int id;
         private string nom;
         private string prenom;
-        private string type;
+        private int type;
         private string adresse;
         private string mail;
-        private int numPortable;
-        private List<Participant> lesParticipants;
+        private int portable;
+        private int idAtelier;
+        private int hreBenevole;
 
-        public Participant(int id, string nom, string prenom, string type, string adresse, string mail, int numPortable)
+        public participant(string nom, string prenom, int type, string adresse, string mail, int portable, int idAtelier, int hreBenevole)
         {
-            this.Id = id;
-            this.Nom = nom;
-            this.Prenom = prenom;
-            this.Type = type;
-            this.Adresse = adresse;
-            this.Mail = mail;
-            this.NumPortable = numPortable;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.type = type;
+            this.adresse = adresse;
+            this.mail = mail;
+            this.portable = portable;
+            this.idAtelier = idAtelier;
+            this.hreBenevole = hreBenevole;
         }
 
-        #region Getter Setter
-        public int Id { get => id; set => id = value; }
-        public string Nom { get => nom; set => nom = value; }
-        public string Prenom { get => prenom; set => prenom = value; }
-        public string Type { get => type; set => type = value; }
-        public string Adresse { get => adresse; set => adresse = value; }
-        public string Mail { get => mail; set => mail = value; }
-        public int NumPortable { get => numPortable; set => numPortable = value; }
-        internal List<Participant> LesParticipants { get => lesParticipants; set => lesParticipants = value; }
-        #endregion
-
-        public static List<Participant> listeParticipant()
+        public string Nom
         {
-            return DAOParticipant.getParticipant();
+            get => nom;
+            set => nom = value;
+        }
+
+        public string Prenom
+        {
+            get => prenom;
+            set => prenom = value;
+        }
+
+        public int Type
+        {
+            get => type;
+            set => type = value;
+        }
+
+        public string Adresse
+        {
+            get => adresse;
+            set => adresse = value;
+        }
+
+        public string Mail
+        {
+            get => mail;
+            set => mail = value;
+        }
+
+        public int Portable
+        {
+            get => portable;
+            set => portable = value;
+        }
+
+        public int IdAtelier
+        {
+            get => idAtelier;
+            set => idAtelier = value;
+        }
+
+        public int HreBenevole
+        {
+            get => hreBenevole;
+            set => hreBenevole = value;
         }
     }
 }
