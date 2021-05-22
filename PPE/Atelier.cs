d509abe81c5fb2923db0510a7e994f28aad0e499
Amaleo1630 <60462731+Amaleo1630@users.noreply.github.com>
2021-05-22 15:03:43 +0200

    -

--- PPE/Atelier.cs
+++ PPE/Atelier.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PPE
 {
-    class Atelier
+    public class Atelier
     {
         private int id;
         private string libelle;
@@ -55,16 +55,11 @@ namespace PPE
 
     }
 
-    class AtelierWithAnimateur
+    class AtelierWithAnimateur : Atelier
     {
-        private int id;
-        private string libelle;
-        private int capaciteMax;
-        private string debut;
-        private string fin;
         private string animateur;
 
-        public AtelierWithAnimateur(int id, string libelle, int capaciteMax, string debut, string fin, string animateur)
+        public AtelierWithAnimateur(int id, string libelle, int capaciteMax, string debut, string fin, string animateur) : base(id, libelle, capaciteMax, debut, fin)
         {
             this.Id = id;
             this.Libelle = libelle;
@@ -75,32 +70,6 @@ namespace PPE
         }
 
         #region Getter Setter
-        public int Id
-        {
-            get => id;
-            set => id = value;
-        }
-
-        public string Libelle
-        {
-            get => libelle;
-            set => libelle = value;
-        }
-        public int CapaciteMax
-        {
-            get => capaciteMax;
-            set => capaciteMax = value;
-        }
-        public string Debut
-        {
-            get => debut;
-            set => debut = value;
-        }
-        public string Fin
-        {
-            get => fin;
-            set => fin = value;
-        }
         public string Animateur { get => animateur; set => animateur = value; }
 
         #endregion
